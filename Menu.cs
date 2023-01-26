96a965a4a77750bc9a35b7cd6e267d367f6cf476
Henrik Vincents Poulsen <hpo@tec.dk>
2023-01-26 12:06:00 +0100

    asdf

--- Menu.cs
+++ Menu.cs
@@ -7,15 +7,26 @@
         //Constructor that runs when object is instantiated (with new keyword).
         public Menu()
         {
+            FakeData();
             while (true)
             {
                 MainMenu();
             }
         }
+
+        private void FakeData()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                ToDoItem tdi = new ToDoItem() { Created = DateTime.Now, Deadline = DateTime.Now.AddDays(1), Description = "Something " + i};
+                todoList.Add(tdi);
+            }
+        }
+
         //Start menu
         public void MainMenu()
         {
-            Console.WriteLine("Main Menu\n\n(1) Add new\n(2) Show List");
+            Console.WriteLine("\nMain Menu\n\n(1) Add new\n(2) Show List\n(3) Update Item\n");
 
             var v = Console.ReadKey(true);
             switch (v.KeyChar)
@@ -25,6 +36,9 @@
                     break;
                 case '2':
                     ShowList();
+                    break;
+                case '3':
+                    UpdateItem();
                     break;
                 default:
                     break;
@@ -47,7 +61,7 @@
             newItem.Created = DateTime.Now;
 
             Console.WriteLine("What to do?");
-            newItem.Todo = Console.ReadLine();
+            newItem.Description = Console.ReadLine();
 
             Console.WriteLine("When to do it?");
             string dl = Console.ReadLine();
@@ -64,14 +78,26 @@
         //Read TodoItem
         private void ShowItem(ToDoItem toDoItem)
         {
-            Console.WriteLine($"What: {toDoItem.Todo} When: {toDoItem.Deadline} isDone: {toDoItem.IsDone}");
+            int i = todoList.IndexOf(toDoItem);
+            Console.WriteLine($"index: {i} \tWhat: {toDoItem.Description} \tWhen: {toDoItem.Deadline} \tisDone: {toDoItem.IsDone}");
             //Console.WriteLine("What: {0} When: {1} isDone: {2}", toDoItem.Todo, toDoItem.Deadline, toDoItem.IsDone);
             //Console.WriteLine("What: " + toDoItem.Todo + "When: " + toDoItem.Deadline + "isDone: " + toDoItem.IsDone);
         }
 
 
         //Update Item
+        void UpdateItem()
+        {
+            Console.WriteLine("Update. Pick item index for todoitem that needs updating");
+            ShowList();
+            int i = int.Parse(Console.ReadLine());
+            ToDoItem tdi = todoList[i];
+            Console.WriteLine("What to do?");
+            string input = Console.ReadLine();
+            if (input != "")
+                tdi.Description = input;
 
+        }
 
         //Delete Item
 
